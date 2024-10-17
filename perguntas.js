e478cc4a4c1d154cd4e6b7e99a2613a1d9a44d17
gustavo gabriel fernandes pedrozo <107936389+gustavogabrielfernandes@users.noreply.github.com>
2024-10-17 10:24:14 -0300

    hvugiu

--- perguntas.js
+++ perguntas.js
@@ -3,17 +3,20 @@ criaCartao(
     'O que é Python?',
     'O Python é uma linguagem de programação'
     )
-    criaCartao(
+
+criaCartao(
     'Geografia',
     'Qual a capital da França?',
     'A capital da França é Paris'
     )
-    criaCartao(
+
+criaCartao(
     'Programação',
     'O que é uma função?',
     'Uma função é um bloco de código que executa alguma tarefa'
     )
-    criaCartao(
+
+ criaCartao(
     'Lingua inglesa',
     'Como se diz oi em Inglês?',
     'Oi em ingles é HI (RAI)'
